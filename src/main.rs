b4e329672707bc29747725722514ba5d95a954fe
JayDalton <github@t-jay.net>
2022-04-08 20:55:45 +0200

    someting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::convert::From;
+use std::fmt;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.value)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    // Try removing the type declaration
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+
+    let circle = Number { value: 6 };
+    println!("{}", circle.to_string());
 }
